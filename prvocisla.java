3a7acd41a9b6836628c155708fb1c820fb1a66b1
Danduny <62833585+Danduny@users.noreply.github.com>
2020-09-08 09:15:43 +0200

    Add files via upload

---
+++ prvocisla.java
@@ -0,0 +1,17 @@
+package Prvocisla;
+
+public class prvocisla {
+    
+    for(int cislo = 1; cislo <= 100; cislo++) {
+        int vypocet = 0;
+        for(int limit = 1; limit <= cislo; limit++) {
+            if(cislo%limit==0) vypocet++;
+
+        }
+        if(vypocet == 2) {
+            System.out.println(cislo);
+
+        }
+    }
+
+}
